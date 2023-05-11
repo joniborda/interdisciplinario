02a80c359b380cb247720571922ced49a0a0dc32
Jonathan Borda <jonathanmatiasborda@gmail.com>
2023-05-11 00:21:23 -0300

    add initial menu

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,77 @@
+import clsx from 'clsx'
+import { useState } from 'react'
+
+export const TopBar = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  return (
+    <div className="topBar flex w-full">
+      <div
+        className={clsx(
+          'z-50 absolute pt-3 pl-2 min-w-[300px] drop-shadow-md h-screen',
+          isMenuOpen ? 'left-0' : '-left-full',
+        )}
+        style={{
+          boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.5)',
+        }}
+      >
+        <button
+          className={clsx(
+            'flex flex-col w-10 h-12 border-0 bg-transparent gap-[0.75rem]',
+            isMenuOpen && 'group',
+          )}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <div
+            className="bg-white h-0.5 w-full rounded-[5px] transition-all group-hover:rotate-45"
+            style={{ transformOrigin: 'left' }}
+          ></div>
+          <div
+            className="bg-white h-0.5 w-full rounded-[5px] transition-all group-hover:opacity-0"
+            style={{ transformOrigin: 'left' }}
+          ></div>
+          <div
+            className="bg-white h-0.5 w-full rounded-[5px] transition-all group-hover:-rotate-45"
+            style={{ transformOrigin: 'left' }}
+          ></div>
+        </button>
+        <div className="flex flex-col gap-1 justify-center items-center">
+          <a
+            className="py-2 hover:bg-slate-800 block w-full font-[fortnite] tracking-widest text-xl"
+            href="/"
+          >
+            ¿Qué disciplinas intervienen?
+          </a>
+          <a
+            className="py-2 hover:bg-slate-800 block w-full font-[fortnite] tracking-widest text-xl"
+            href="/guess"
+          >
+            ¿Cuál es la disciplina?
+          </a>
+        </div>
+      </div>
+      <div className="pt-3 px-2">
+        <button
+          className={clsx(
+            'flex flex-col w-10 h-12 border-0 bg-transparent gap-[0.75rem]',
+            isMenuOpen && 'group',
+          )}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <div
+            className="bg-white h-0.5 w-full rounded-[5px] transition-all group-hover:rotate-45"
+            style={{ transformOrigin: 'left' }}
+          ></div>
+          <div
+            className="bg-white h-0.5 w-full rounded-[5px] transition-all group-hover:opacity-0"
+            style={{ transformOrigin: 'left' }}
+          ></div>
+          <div
+            className="bg-white h-0.5 w-full rounded-[5px] transition-all group-hover:-rotate-45"
+            style={{ transformOrigin: 'left' }}
+          ></div>
+        </button>
+      </div>
+      <div className="grow">{children}</div>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,29 @@
 import { Heading1 } from "@/components/Heading1";
-import { CareersInvolvedBody } from "../components/CareersInvolvedBody";
+import { CareersInvolvedBody } from "@/components/CareersInvolvedBody";
+import { TopBar } from "@/components/TopBar";
+
 import clsx from "clsx";
 
 export default function Home() {
   return (
-    <main
-      className={clsx(
-        "flex h-screen overflow-hidden flex-col items-center justify-between",
-        "px-1 sm:px-2 md:px-1 pt-10 sm:pb-5"
-      )}
-    >
-      <div className="z-10 h-full w-full max-w-5xl items-center flex-col justify-between text-sm lg:flex">
+    <main>
+      <TopBar>
         <Heading1
           style={{ textShadow: "1px 4px 4px #000000" }}
           className="font-[fortnite] text-4xl"
         >
           ¿Qué disciplinas intervienen?
         </Heading1>
-        <CareersInvolvedBody />
+      </TopBar>
+      <div
+        className={clsx(
+          "flex h-screen overflow-hidden flex-col items-center justify-between",
+          "px-1 sm:px-2 md:px-1 pt-10 sm:pb-5"
+        )}
+      >
+        <div className="z-10 h-full w-full max-w-5xl items-center flex-col justify-between text-sm lg:flex">
+          <CareersInvolvedBody />
+        </div>
       </div>
     </main>
   );
