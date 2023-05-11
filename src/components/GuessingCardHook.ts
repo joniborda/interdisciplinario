d12afe3f02048061516a065352af30a906cd6523
Jonathan Borda <jonathanmatiasborda@gmail.com>
2023-05-11 09:30:48 -0300

    fix touch method

--- src/components/GuessingCardHook.ts
+++ src/components/GuessingCardHook.ts
@@ -1,41 +1,40 @@
 import { use, useEffect, useRef } from "react";
 
+const getX = (e: DragEvent | TouchEvent) => {
+  if ("clientX" in e) {
+    return e.clientX;
+  }
+  return e.touches[0].clientX;
+};
 export const useGuessingCard = ({ handleLikeCard, handleDislikeCard, frontSide }: any) => {
   const refCard = useRef<HTMLDivElement>(null);
   const maxDragToChangePage = 150;
   let dragXCurrent = 0;
   let initialDragX = 0;
 
-  const dragStart = (e: DragEvent) => {
+  const dragStart = (e: DragEvent | TouchEvent) => {
     const card = refCard.current;
     if (card) {
       card.style.transform = `translateX(0px)`;
-      initialDragX = e.clientX;
+      initialDragX = getX(e);
 
-      const img = new Image();
-      e?.dataTransfer?.setDragImage(img, 0, 0);
+      if ("dataTransfer" in e) {
+        const img = new Image();
+        e?.dataTransfer?.setDragImage(img, 0, 0);
+      }
     }
   };
 
-  const dragStartMobile = (e: TouchEvent) => {
-    //TODO: refactor this
-    const card = refCard.current;
-    if (card) {
-      card.style.transform = `translateX(0px)`;
-      initialDragX = e.touches[0].clientX;
-
-      const img = new Image();
-    }
-  };
-
-  const dragElement = (e: DragEvent) => {
+  const dragElement = (e: DragEvent | TouchEvent) => {
     if (frontSide) return;
 
     const card = refCard.current;
     // stay default cursor
-    if (e.dataTransfer) e.dataTransfer.dropEffect = "none";
+    if ("dataTransfer" in e && e.dataTransfer) {
+      e.dataTransfer.dropEffect = "none";
+    }
 
-    const x = e.clientX;
+    const x = getX(e);
     if (card && x !== 0) {
       const dragX = x - initialDragX;
       dragXCurrent = dragX;
@@ -48,25 +47,6 @@ export const useGuessingCard = ({ handleLikeCard, handleDislikeCard, frontSide }
     }
   };
 
-  const dragElementMobile = (e: TouchEvent) => {
-    //TODO: refactor this
-    if (frontSide) return;
-
-    const card = refCard.current;
-    // stay default cursor
-
-    const x = e.touches[0].clientX;
-    if (card && x !== 0) {
-      const dragX = x - initialDragX;
-      dragXCurrent = dragX;
-      card.style.transform = `rotate(${dragX / 50}deg) translateX(${dragX}px)`;
-
-      card.style.opacity = "1";
-      if (maxDragToChangePage < dragXCurrent || dragXCurrent < -maxDragToChangePage) {
-        card.style.opacity = "0.8";
-      }
-    }
-  };
   const resetPosition = () => {
     const card = refCard.current;
     if (card) {
@@ -87,8 +67,8 @@ export const useGuessingCard = ({ handleLikeCard, handleDislikeCard, frontSide }
       card.addEventListener("dragstart", dragStart);
       card.addEventListener("drag", dragElement);
       card.addEventListener("dragend", resetPosition);
-      card.addEventListener("touchstart", dragStartMobile);
-      card.addEventListener("touchmove", dragElementMobile);
+      card.addEventListener("touchstart", dragStart);
+      card.addEventListener("touchmove", dragElement);
       card.addEventListener("touchend", resetPosition);
     }
     return () => {
@@ -96,8 +76,8 @@ export const useGuessingCard = ({ handleLikeCard, handleDislikeCard, frontSide }
         card.removeEventListener("dragstart", dragStart);
         card.removeEventListener("drag", dragElement);
         card.removeEventListener("dragend", resetPosition);
-        card.removeEventListener("touchstart", dragStartMobile);
-        card.removeEventListener("touchmove", dragElementMobile);
+        card.removeEventListener("touchstart", dragStart);
+        card.removeEventListener("touchmove", dragElement);
         card.removeEventListener("touchend", resetPosition);
       }
     };
