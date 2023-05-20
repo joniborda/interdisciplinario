ac40652dc0331f72c143d564d9a0067c27bf6b88
Jonathan Borda <jonathanmatiasborda@gmail.com>
2023-05-19 22:03:27 -0300

    improve search list

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -1,8 +1,8 @@
 import clsx from "clsx";
 import { useState } from "react";
 
-export default function InputSearch() {
-    const [onFocus, setOnFocus] = useState(false);
+export default function InputSearch({ onChange }: { onChange: (e: any) => void }) {
+  const [onFocus, setOnFocus] = useState(false);
   return (
     <>
       <div className="flex justify-center">
@@ -10,17 +10,25 @@ export default function InputSearch() {
           <input
             type="search"
             name="q"
-            className="py-2 text-sm text-white bg-gray-800 rounded-md pl-2 pr-10 focus:outline-none focus:bg-white focus:text-gray-800"
+            onChange={onChange}
+            className={clsx(
+              "py-2 text-white bg-gray-800 rounded-full pl-4 pr-10",
+              "focus:outline-none focus:bg-white focus:text-gray-800",
+              "min-w-[300px]"
+            )}
             placeholder="Buscar..."
             autoComplete="off"
             onFocus={() => setOnFocus(true)}
             onBlur={() => setOnFocus(false)}
           />
-          <button type="submit" 
-          className={clsx("absolute right-0 p-1 pt-1.5 block rounded-md ",
-          onFocus ? "bg-white text-gray-800": "text-white bg-gray-800"
-          )}>
-            <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current stroke-current">
+          <button
+            type="submit"
+            className={clsx(
+              "absolute right-2 p-1 pt-1.5 block rounded-r-full ",
+              onFocus ? "bg-white text-gray-800" : "text-white bg-gray-800"
+            )}
+          >
+            <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current stroke-none">
               <g>
                 <path d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z"></path>
               </g>
--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -2,8 +2,22 @@ import { Heading1 } from "@/components/Heading1";
 import InputSearch from "@/components/InputSearch";
 import { TopBar } from "@/components/TopBar";
 import clsx from "clsx";
+import data from "@/data/guessing.json";
+import { useState } from "react";
 
 export default function List() {
+  const [search, setSearch] = useState<string>("");
+  const [filteredData, setFilteredData] = useState(data);
+  const onChangeSearch = (e: any) => {
+    const newSearch = e.target.value;
+    const newData = data.filter((item) => {
+      if (newSearch === "") return true;
+      const newItemWithoutAccent = item.title.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      if (newItemWithoutAccent.toLowerCase().includes(newSearch.toLowerCase())) return true;
+    });
+    setSearch(newSearch);
+    setFilteredData(newData);
+  };
   return (
     <main className="h-screen">
       <TopBar>
@@ -15,16 +29,20 @@ export default function List() {
         </Heading1>
       </TopBar>
       <div className="mt-2">
-        <InputSearch />
+        <InputSearch onChange={onChangeSearch} />
       </div>
       <div
         className={clsx(
-          "flex h-[calc(100vh-102px)] overflow-hidden flex-col",
+          "grid overflow-hidden grid-cols-3",
           "items-center justify-between p-6 sm:p-20 pt-1 sm:pt-10"
         )}
       >
-        <div className="relative z-10 h-full w-full max-w-5xl items-center justify-between text-sm lg:flex mt-4">
-        </div>
+        {filteredData.map((item) => (
+          <div className="">
+            <h2 className="text-center text-lg font-[fortnite] tracking-widest">{item.title}</h2>
+            <p className="max-h-[50px] overflow-hidden text-ellipsis">{item.riddle}</p>
+          </div>
+        ))}
       </div>
     </main>
   );
